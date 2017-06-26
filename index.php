4ef957628291af725594762629f07d4e8b797178
孫裕軒 <jk2626173jk.eed03@nctu.edu.tw>
2017-06-27 04:00:37 +0800

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   
   <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Francois+One|Marck+Script|Source+Sans+Pro'>
 
-      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="style.css">
 
   
 </head>
@@ -148,7 +148,7 @@
   <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.28/vue.js'></script>
 
-    <script src="js/index.js"></script>
+    <script src="index.js"></script>
 
 </body>
 </html>
